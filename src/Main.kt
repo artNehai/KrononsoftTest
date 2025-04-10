687a498adb30352fc8f2c2632ed337dcc2edf410
artNehay <artemynehay@gmail.com>
2025-04-10 12:30:16 +0300

    Implement the main logic

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,69 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println(1 + 2)
+data class Workplace(
+    val x: Int,
+    val y: Int,
+)
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+enum class Direction(val symbol: Char) {
+    UP('U'),
+    RIGHT('R'),
+    DOWN('D'),
+    LEFT('U'),
+}
+
+class Round(
+    val startingPosition: Workplace,
+) {
+    private val workplaceToVisitsMap = mutableMapOf<Workplace, Int>()
+    private var currentPositionX = startingPosition.x
+    private var currentPositionY = startingPosition.y
+
+    fun start() {
+        val path = readlnOrNull()
+            ?.uppercase()
+            ?.filter { direction ->
+                direction in Direction.entries.map(Direction::symbol)
+            }
+            ?.take(MAX_PATH_LENGTH)
+            ?: return
+
+        for (direction in path) {
+            when (direction) {
+                Direction.UP.symbol -> currentPositionY++
+                Direction.DOWN.symbol -> currentPositionY--
+                Direction.LEFT.symbol -> currentPositionX--
+                Direction.RIGHT.symbol -> currentPositionX++
+            }
+
+            val newDestination = Workplace(currentPositionX, currentPositionY)
+            workplaceToVisitsMap[newDestination] = (workplaceToVisitsMap[newDestination] ?: 0) + 1
+        }
+
+        val result = workplaceToVisitsMap.count { it.value > 1 }
+        println("Number of workplaces visited more than once: $result")
+    }
+
+    companion object {
+        private const val MAX_PATH_LENGTH = 1000
+    }
+}
+
+fun main() {
+
+    println("Choose the starting position:")
+    val x = readPositiveInt("x = ")
+    val y = readPositiveInt("y = ")
+
+    val round = Round(startingPosition = Workplace(x, y))
+    round.start()
+}
+
+fun readPositiveInt(accompanyingText: String = ""): Int {
+    print(accompanyingText)
+    while (true) {
+        try {
+            return readln().toUInt().toInt()
+        } catch (e: NumberFormatException) {
+            print("Invalid value. Please enter whole positive number:")
+        }
     }
 }
